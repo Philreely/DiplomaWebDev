e6ab7b345d321d4d43787303662bff5d92defd31
Philip Reely <Philreely@users.noreply.github.com>
2020-03-04 22:10:34 +0000

    Update index.php

--- index.php
+++ index.php
@@ -6,9 +6,27 @@
   
   <body>
     <?php
-       echo "this writes to html doc";
-
-
+      echo "<p>this writes to html doc</p>";
+      echo 5+5;
+      echo "string 1"."string 2"
+     
+        $myString = "this is a string";
+        echo $myString;
+    
+        if (4<5) {
+          echo "four is less than five";
+        }
+        else {
+          echo "the comparison is false";
+        }
+        
+        for ($i=0;$i<5;$i++){
+          echo "<p>this is the ".$i."th run of the for loop</p>";
+        }
+    
+        $myAnimals = array ("ant", "bear", "cow");
+        for ($i=0;$i<3;$i++){
+          echo "<p>the element in the array is: ".$myAnimals[$i}."</p>"
 
 
 
